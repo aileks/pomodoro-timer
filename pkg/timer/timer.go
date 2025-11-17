911bb1e60102714333ac424161491c23fa8c2ddf
Aaliyah Harper <aaliyah@aileks.dev>
2025-11-16 20:04:16 -0500

    finish pomo timer logic

--- pkg/timer/timer.go
+++ pkg/timer/timer.go
@@ -24,14 +24,46 @@ func New(duration time.Duration) *PomoTimer {
 	}
 }
 
-func Start() {
-	// TODO
+func (t *PomoTimer) Start() {
+	if t.state == StateRunning {
+		return
+	}
+
+	t.deadline = time.Now().Add(t.remaining)
+	t.state = StateRunning
+	t.timer = time.NewTimer(t.remaining)
 }
 
-func Pause() {
-	// TODO
+func (t *PomoTimer) Pause() {
+	if t.state == StatePaused {
+		return
+	}
+
+	t.timer.Stop()
+	t.remaining = time.Until(t.deadline)
+	t.state = StatePaused
 }
 
-func Resume() {
-	// TODO
+func (t *PomoTimer) Resume() {
+	if t.state != StatePaused {
+		return
+	}
+
+	t.Start()
+}
+
+func (t *PomoTimer) Remaining() time.Duration {
+	if t.state == StateRunning {
+		return time.Until(t.deadline)
+	}
+
+	return t.remaining
+}
+
+func (t *PomoTimer) IsFinished() bool {
+	if t.state == StateRunning {
+		return time.Now().After(t.deadline)
+	}
+
+	return t.remaining <= 0
 }
